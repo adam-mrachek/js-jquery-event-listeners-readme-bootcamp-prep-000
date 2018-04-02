51c750a8ffc56c942b27ced247d3cb83af6614c7
Adam Mrachek <amrachek@outlook.com>
2018-04-02 21:38:41 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,13 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });ß
+}
 
 $(document).ready(function(){
 
 // call functions here
+  getIt();
 
 });
